583c9478230862ef90c034e5cb804491810fb409
Michael Gavin <michael.alex.gavin@gmail.com>
2018-03-20 15:40:21 -0700

    Debugged gradient descent function and finalized Ridge capabilities

--- data_tools/log_reg.py
+++ data_tools/log_reg.py
@@ -8,9 +8,9 @@ def log_loss(predictions,actual,eps=1e-15):
     also factoring for confidence'''
     #assert (max(predictions)<=1 and min(predictions)>=0), 'Please make sure to use predict_proba'
     
-    preds_clipped = np.clip(predictions,eps,1-eps)
+    p_clipped = np.clip(predictions,eps,1-eps)
     
-    loss = -1 * np.mean((actual * np.log(preds_clipped)) + ((1-actual) * np.log(1-preds_clipped)))
+    loss = -1 * np.mean((actual * np.log(p_clipped)) + ((1-actual) * np.log(1-p_clipped)))
     
     return loss
 
@@ -21,9 +21,9 @@ def sigmoid(array):
 class BinaryClassifier:
     
     def __init__(self,regularization=None):
-        '''initializing the object with the option to select regularization'''
+        '''initializing the object with the option to select regularization
         
-        '''Regularization will be a dict with type and lambda value'''
+        Regularization will be a dict with type (ridge/lasso) and lambda value'''
         
         if regularization is None:
             self.penalty_type = None
@@ -33,7 +33,7 @@ class BinaryClassifier:
             self.penalty_type = list(regularization.keys())[0]
             self.penalty_lambda_ = regularization.get(self.penalty_type)
     
-    def _gradient_descent(self,X,y,lr=.1,pandas=False,full_history=False,weights=None):
+    def _gradient_descent(self,X,y,lr=.1,pandas=False,full_history=False,weights=None,early_stopping=True):
         
         if pandas or (isinstance(X,pd.DataFrame) & isinstance(y,pd.DataFrame)):
             self.X = X.values
@@ -42,7 +42,7 @@ class BinaryClassifier:
             self.ynames = y.columns
         else:
             self.X = X
-            self.y = y.reshape(len(y),1)
+            self.y = y
             self.Xnames = [i for i in range(X.shape[1])]
             
             
@@ -56,17 +56,17 @@ class BinaryClassifier:
         '''creating the weights, which will typically be all zeros'''
             
         if weights is None:
-            self.weights = np.zeros(n_features)
+            self.init_weights = np.zeros(n_features)
         else:
-            self.weights = weights
+            self.init_weights = weights
             
         if self.penalty_type is 'lasso':
             reg_loss = (self.penalty_lambda_)
             reg_gradient = (self.penalty_lambda_)
         
         elif self.penalty_type is 'ridge':
-            reg_loss = ((self.penalty_lambda/2) * np.square(self.weights))/m
-            reg_gradient = (self.penalty_lambda_/m)
+            reg_loss = (self.penalty_lambda_/2)# * np.square(self.init_weights))/m
+            reg_gradient = (-2*self.penalty_lambda_/m)
             
         else:
             reg_loss = 0
@@ -75,9 +75,21 @@ class BinaryClassifier:
         weights_list = []
         scores_list = []
         
+        weights = self.init_weights
+        
         for i in range(1000):
             
-            weights = self.weights
+            if self.penalty_type is 'ridge':
+                gradient_suffix = reg_gradient * weights
+                loss_suffix = np.sum(reg_loss * np.square(weights)/m)
+                
+            elif self.penalty_type is 'lasso':
+                pass
+            
+            else:
+                gradient_suffix = 0
+                loss_suffix = 0
+            
             X = self.X
             lr = self.lr
             y = self.y
@@ -90,44 +102,49 @@ class BinaryClassifier:
             
             gradient = (np.dot(X.T,error) * lr) /m 
             
-            weights = weights - gradient #/m + (reg_gradient)
+            weights = weights - gradient + gradient_suffix
             
             p = sigmoid(np.dot(X, weights))
             
             preds = np.round(p)
             
-            loss = log_loss(p, y) + (reg_loss)
+            loss = log_loss(p, y) + loss_suffix
             
             auc = roc_auc_score(y, p)
              
             acc = accuracy_score(y,preds)
             
-            weights_list.append(*weights)
+            weights_list.append([*weights])
             
-            scores_list.append(auc,loss,acc)
+            scores_list.append([auc,loss,acc])
             
             '''Early Stopping: if AUC does not change more than 0.01%, then break'''
-            
-            if i >50:
-                if abs((scores_list[i][-3] - scores_list[i-50][-3]) / scores_list[i][-3]) < 0.0001:
-                    break
+            if early_stopping:
+                if i >50:
+                    if abs((scores_list[i][-3] - scores_list[i-50][-3]) / scores_list[i][-3]) < 0.0001:
+                        break
        
-        scores_df = pd.DataFame(scores_list,columns=['auc','loss','auc'])
+        scores_df = pd.DataFrame(scores_list,columns=['auc','loss','acc'])
         
         '''Finding the index with highest AUC score'''
         
-        ID = scores_df.iloc[:,0].idxmax(axis=0)
+        highest_auc = scores_df.iloc[:,0].idxmax(axis=0)
         
-        final_weights = weights_list[ID]
-    #self.weights_final = weights_list[ID]
+        final_weights = weights_list[highest_auc]
+        
+        self.weights_final = weights_list[highest_auc]
         
         weights_df = pd.DataFrame(weights_list,columns=self.Xnames)
         
         full_df = pd.concat([weights_df,scores_df],axis=1)
         
+        self.final_weights, self.full_history = final_weights, full_df
+        
+        print('this test worked!')
+        
         return final_weights, full_df
             
-        print('this test worked!')
+        
             
         
         
